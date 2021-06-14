cfa55c2c7d5fe14f012533fb8b95438b94831bb3
Sevrep <62856034+Sevrep@users.noreply.github.com>
2021-06-15 04:10:26 +0800

    Add toolbar, tablayout and fragments

--- app/src/main/java/com/sevrep/myinstagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/sevrep/myinstagramclone/SocialMediaActivity.java
@@ -3,12 +3,33 @@ package com.sevrep.myinstagramclone;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class SocialMediaActivity extends AppCompatActivity {
+
+    private Toolbar toolbarSocialMedia;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        setTitle(getString(R.string.social_media_app));
+
+        toolbarSocialMedia = findViewById(R.id.toolbarSocialMedia);
+        setSupportActionBar(toolbarSocialMedia);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
+
     }
 }
